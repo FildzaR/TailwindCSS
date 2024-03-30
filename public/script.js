6bc928d6104cb26bfd30e55b4ed56a7ad87b99c8
FildzaR <fildzarasyika@students.usu.ac.id>
2024-03-31 01:08:22 +0700

    commit

--- public/script.js
+++ public/script.js
@@ -1,7 +1,6 @@
 function Menu(e) {
     let list = document.querySelector('.slide-navbar');
     let nav = document.querySelector('.nav');
-    let body = document.querySelector('body');
     let overlay = document.querySelector('.overlay');
 
     e.name === 'menu-outline' ? (e.name = "close-outline", list.classList.add('top-0'), list.classList.add('z-30'), list.classList.remove('-translate-y-full'), nav.classList.add('text-black'), nav.classList.remove('text-white'), nav.classList.add('mr-4'), overlay.classList.remove('hidden')) :(e.name = "menu-outline", list.classList.remove('top-0'), list.classList.remove('z-30'), nav.classList.remove('text-black'), nav.classList.add('text-white'), list.classList.add('-translate-y-full'),overlay.classList.add('hidden'))
